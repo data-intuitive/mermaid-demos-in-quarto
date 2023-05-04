65fc136918dd349b4b271b7245ee56a306946574
Robrecht Cannoodt <rcannood@gmail.com>
2023-05-04 13:18:16 +0200

    update

--- _script.R
+++ _script.R
@@ -7,6 +7,7 @@ library(glue)
 temp_dir <- tempfile(pattern = "mermaid")
 on.exit(unlink(temp_dir, recursive = TRUE), add = TRUE)
 system(paste0("git clone git@github.com:mermaid-js/mermaid.git ", temp_dir))
+# todo: check out tag v9.3.0
 
 demo_files <- list.files(
   paste0(temp_dir, "/demos"),
@@ -64,7 +65,10 @@ if (!dir.exists("syntax")) dir.create("syntax")
 for (file in syntax_files) {
   lines <- readr::read_lines(file)
 
+  # 
   new_lines <- gsub("```mermaid-example", "```{mermaid}", lines)
+  # remove titles in codeblocks
+  new_lines <- gsub("```\\{mermaid\\}\n---\ntitle:[^-]*\n---", "```{mermaid}", paste(new_lines, collapse = "\n"))
 
   dest_file <- paste0("syntax/", gsub("\\.md", ".qmd", basename(file)))
   readr::write_lines(new_lines, dest_file)
